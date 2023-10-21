731b8e05a66dd2994ac2473bb90c85d4fea91999
Muhammad Qadeer <qadeeransari38@gmail.com>
2023-10-22 00:06:34 +0500

    feat: Project files added!

---
+++ Employee.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EAD_Assignment_05
+{
+    public class Employee
+    {
+        private readonly string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"E:\\BscsPUCIT\\BSCS 7th Semester\\EAD\\Assignments\\Assignment05\\EAD Assignment-05\\EAD Assignment-05\\AssignmentFive.mdf\";Integrated Security=True";
+
+        public DataTable GetAllEmployees()
+        {
+            // Estabishing the new Connection.
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Employees";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    DataTable dataTable = new DataTable();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        dataTable.Load(reader);
+                    }
+                    return dataTable;
+                }
+            }
+        }
+
+        public void InsertEmployee(string firstName, string lastName, string email, string primaryPhone, string createdBy, DateTime createdOn)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "INSERT INTO Employees (FirstName, LastName, Email, PrimaryPhoneNumber, CreatedBy, CreatedOn) " +
+                               "VALUES (@FirstName, @LastName, @Email, @PrimaryPhoneNumber, @CreatedBy, @CreatedOn)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@FirstName", firstName);
+                    command.Parameters.AddWithValue("@LastName", lastName);
+                    command.Parameters.AddWithValue("@Email", email);
+                    command.Parameters.AddWithValue("@PrimaryPhoneNumber", primaryPhone);
+                    command.Parameters.AddWithValue("@CreatedBy", createdBy);
+                    command.Parameters.AddWithValue("@CreatedOn", createdOn);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteEmployee(long employeeId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "DELETE FROM Employees WHERE ID = @EmployeeId";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@EmployeeId", employeeId);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public DataRow GetEmployeeById(long employeeId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Employees WHERE ID = @EmployeeId";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@EmployeeId", employeeId);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        DataTable dataTable = new DataTable();
+                        dataTable.Load(reader);
+                        return dataTable.Rows.Count > 0 ? dataTable.Rows[0] : null;
+                    }
+                }
+            }
+        }
+
+        public void UpdateEmployee(long employeeId, string firstName, string lastName, string email, string primaryPhone, string modifiedBy, DateTime? modifiedOn)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "UPDATE Employees " +
+                               "SET FirstName = @FirstName, LastName = @LastName, Email = @Email, " +
+                               "PrimaryPhoneNumber = @PrimaryPhoneNumber, ModifiedBy = @ModifiedBy, " +
+                               "ModifiedOn = @ModifiedOn " +
+                               "WHERE ID = @EmployeeId";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@EmployeeId", employeeId);
+                    command.Parameters.AddWithValue("@FirstName", firstName);
+                    command.Parameters.AddWithValue("@LastName", lastName);
+                    command.Parameters.AddWithValue("@Email", email);
+                    command.Parameters.AddWithValue("@PrimaryPhoneNumber", primaryPhone);
+                    command.Parameters.AddWithValue("@ModifiedBy", modifiedBy);
+                    command.Parameters.AddWithValue("@ModifiedOn", modifiedOn ?? (object)DBNull.Value);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Implemantation using data Adapters
+
+
+
+
+        public DataTable GetAllEmployeesWithDataAdapter()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Employees";
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+
+        public void InsertEmployeeWithDataAdapter(string firstName, string lastName, string email, string primaryPhone, string createdBy, DateTime createdOn)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "INSERT INTO Employees (FirstName, LastName, Email, PrimaryPhoneNumber, CreatedBy, CreatedOn) " +
+                               "VALUES (@FirstName, @LastName, @Email, @PrimaryPhoneNumber, @CreatedBy, @CreatedOn)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@FirstName", firstName);
+                    command.Parameters.AddWithValue("@LastName", lastName);
+                    command.Parameters.AddWithValue("@Email", email);
+                    command.Parameters.AddWithValue("@PrimaryPhoneNumber", primaryPhone);
+                    command.Parameters.AddWithValue("@CreatedBy", createdBy);
+                    command.Parameters.AddWithValue("@CreatedOn", createdOn);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteEmployeeWithDataAdapter(long employeeId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "DELETE FROM Employees WHERE ID = @EmployeeId";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@EmployeeId", employeeId);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public DataTable GetEmployeeByIdWithDataAdapter(long employeeId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Employees WHERE ID = @EmployeeId";
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                {
+                    adapter.SelectCommand.Parameters.AddWithValue("@EmployeeId", employeeId);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+
+        public void UpdateEmployeeWithDataAdapter(long employeeId, string firstName, string lastName, string email, string primaryPhone, string modifiedBy, DateTime? modifiedOn)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "UPDATE Employees " +
+                               "SET FirstName = @FirstName, LastName = @LastName, Email = @Email, " +
+                               "PrimaryPhoneNumber = @PrimaryPhoneNumber, ModifiedBy = @ModifiedBy, " +
+                               "ModifiedOn = @ModifiedOn " +
+                               "WHERE ID = @EmployeeId";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@EmployeeId", employeeId);
+                    command.Parameters.AddWithValue("@FirstName", firstName);
+                    command.Parameters.AddWithValue("@LastName", lastName);
+                    command.Parameters.AddWithValue("@Email", email);
+                    command.Parameters.AddWithValue("@PrimaryPhoneNumber", primaryPhone);
+                    command.Parameters.AddWithValue("@ModifiedBy", modifiedBy);
+                    command.Parameters.AddWithValue("@ModifiedOn", modifiedOn ?? (object)DBNull.Value);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using EAD_Assignment_05;
+using System;
+using System.Data;
+
+namespace EAD_Assignment_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee dataAccess = new Employee();
+
+            Console.WriteLine("Select Data Access Method:");
+            Console.WriteLine("1. Perform CRUD with SqlDataReader");
+            Console.WriteLine("2. Perform CRUD with SqlDataAdapter");
+            Console.Write("Enter your choice: ");
+
+            if (int.TryParse(Console.ReadLine(), out int dataAccessChoice))
+            {
+                if (dataAccessChoice == 1)
+                {
+                    RunCRUDWithSqlDataReader(dataAccess);
+                }
+                else if (dataAccessChoice == 2)
+                {
+                    RunCRUDWithSqlDataAdapter(dataAccess);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please try again.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+
+        static void RunCRUDWithSqlDataReader(Employee dataAccess)
+        {
+            while (true)
+            {
+                Console.WriteLine("Employee Database Menu with SqlDataReader:");
+                Console.WriteLine("1. List All Employees");
+                Console.WriteLine("2. Add Employee");
+                Console.WriteLine("3. Delete Employee");
+                Console.WriteLine("4. Update Employee");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            GetAllEmployees(dataAccess);
+                            break;
+                        case 2:
+                            InsertEmployee(dataAccess);
+                            break;
+                        case 3:
+                            DeleteEmployee(dataAccess);
+                            break;
+                        case 4:
+                            UpdateEmployee(dataAccess);
+                            break;
+                        case 5:
+                            return;
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void RunCRUDWithSqlDataAdapter(Employee dataAccess)
+        {
+            while (true)
+            {
+                Console.WriteLine("Employee Database Menu with SqlDataAdapter:");
+                Console.WriteLine("1. List All Employees");
+                Console.WriteLine("2. Add Employee");
+                Console.WriteLine("3. Delete Employee");
+                Console.WriteLine("4. Update Employee");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            ListAllEmployeesWithDataAdapter(dataAccess);
+                            break;
+                        case 2:
+                            AddEmployeeWithDataAdapter(dataAccess);
+                            break;
+                        case 3:
+                            DeleteEmployeeWithDataAdapter(dataAccess);
+                            break;
+                        case 4:
+                            UpdateEmployeeWithDataAdapter(dataAccess);
+                            break;
+                        case 5:
+                            return;
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
