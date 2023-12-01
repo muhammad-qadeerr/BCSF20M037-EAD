6574cf3f1c67f275575b30317a6131a0b02442c5
Muhammad Qadeer <qadeeransari38@gmail.com>
2023-12-01 23:38:05 +0500

    feat: Assignment 8 files

---
+++ Interpreter.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Abstract Expression
+public interface IDateExpression
+{
+    DateTime Interpret();
+}
+
+// Terminal Expression 1: Year
+public class YearExpression : IDateExpression
+{
+    private readonly int _year;
+
+    public YearExpression(int year)
+    {
+        _year = year;
+    }
+
+    public DateTime Interpret()
+    {
+        return new DateTime(_year, 1, 1);
+    }
+}
+
+// Terminal Expression 2: Month
+public class MonthExpression : IDateExpression
+{
+    private readonly IDateExpression _year;
+    private readonly int _month;
+
+    public MonthExpression(IDateExpression year, int month)
+    {
+        _year = year;
+        _month = month;
+    }
+
+    public DateTime Interpret()
+    {
+        DateTime yearDate = _year.Interpret();
+        return new DateTime(yearDate.Year, _month, 1);
+    }
+}
+
+// Terminal Expression 3: Day
+public class DayExpression : IDateExpression
+{
+    private readonly IDateExpression _month;
+    private readonly int _day;
+
+    public DayExpression(IDateExpression month, int day)
+    {
+        _month = month;
+        _day = day;
+    }
+
+    public DateTime Interpret()
+    {
+        DateTime monthDate = _month.Interpret();
+        return new DateTime(monthDate.Year, monthDate.Month, _day);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Interpret the date expression: year(2023), month(11), day(29)
+        IDateExpression expression = new DayExpression(
+            new MonthExpression(
+                new YearExpression(2023),
+                11),
+            29);
+
+        DateTime date = expression.Interpret();
+        Console.WriteLine($"Interpreted Date: {date}");
+        DateTime date1 = expression.Interpret();
+        Console.WriteLine($"Interpreted Date: {date1}");
+
+        //Console.ReadLine();
+    }
+}
---
+++ Startegy.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ass8
+{
+    // The Context defines the interface of interest to clients.
+    class Context
+    {
+        private IStrategy _strategy;
+
+        public Context()
+        { }
+
+        // Usually, the Context accepts a strategy through the constructor, but
+        // also provides a setter to change it at runtime.
+        public Context(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+        public void SetStrategy(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+        public void DoSomeBusinessLogic()
+        {
+            Console.WriteLine("Context: Sorting data using the strategy (not sure how it'll do it)");
+            var result = this._strategy.DoAlgorithm(new List<string> { "a", "b", "c", "d", "e" });
+
+            string resultStr = string.Empty;
+            foreach (var element in result as List<string>)
+            {
+                resultStr += element + ",";
+            }
+
+            Console.WriteLine(resultStr);
+        }
+    }
+
+    // The Strategy interface declares operations common to all supported
+    // versions of some algorithm.
+    //
+    // The Context uses this interface to call the algorithm defined by Concrete
+    // Strategies.
+    public interface IStrategy
+    {
+        object DoAlgorithm(object data);
+    }
+
+    class ConcreteStrategyA : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+
+            return list;
+        }
+    }
+
+    class ConcreteStrategyB : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+            list.Reverse();
+
+            return list;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code picks a concrete strategy and passes it to the
+            // context. The client should be aware of the differences between
+            // strategies in order to make the right choice.
+            var context = new Context();
+
+            Console.WriteLine("Client: Strategy is set to normal sorting.");
+            context.SetStrategy(new ConcreteStrategyA());
+            context.DoSomeBusinessLogic();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Strategy is set to reverse sorting.");
+            context.SetStrategy(new ConcreteStrategyB());
+            context.DoSomeBusinessLogic();
+        }
+    }
+}
